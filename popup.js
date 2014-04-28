52ceaf359ee47df18ec577e8033e2b31a1260610
Marush Denchev <avreon@gmail.com>
2014-04-28 22:12:24 +0300

    Arrange code. Simple way to detect if file exists.

--- popup.js
+++ popup.js
@@ -4,38 +4,46 @@ function make_base_auth(user, password) {
   return "Basic " + hash;
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+var username = 'devadmin',
+      password = 'sofia2010';
 
-  var username = 'devadmin',
-      password = 'sofia2010'
 
-  $.ajax
-({
-  type: "GET",
-  url: "https://dev30.web03.ecommera.demandware.net/on/demandware.servlet/webdav/Sites/Logs/error-blade1-2-appserver-20140428.log",
-  beforeSend: function (xhr){ 
+function do_notifications() {
+
+  var d = new Date(), year = d.getFullYear(), month = ('0' + (d.getMonth()+1)).slice(-2),date = d.getDate();
+
+  $.ajax({
+    type: "GET",
+    url: "https://dev30.web03.ecommera.demandware.net/on/demandware.servlet/webdav/Sites/Logs/error-blade1-2-appserver-" + year + month + date + ".log",
+    beforeSend: function (xhr){ 
         xhr.setRequestHeader('Authorization', make_base_auth(username, password)); 
     },
-  success: function (response){
-    var regex = new RegExp(/\[\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+ GMT]/);
+    error : function() {
+      console.log('File does not exists.');
+    },
+    success: function (response){
+      console.log('File exists.');
+      var regex = new RegExp(/\[\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+ GMT]/);
 
-   var errors = response.split(regex);
-   var date = new Date();
-   var notifId = "demandware_" + date.getFullYear() + '_' + date.getMonth() + '_' + date.getDate() + '_' + errors.length + Math.random();
+      var errors = response.split(regex);
+      var date = new Date();
+      var notifId = "demandware_" + date.getFullYear() + '_' + date.getMonth() + '_' + date.getDate() + '_' + errors.length + Math.random();
 
-   chrome.notifications.create(notifId, {
-    title : "New error",
-    message : errors.pop(),
-    type : "basic",
-    iconUrl : "icon.png"
-   }, function() {
-    console.log('Creation callback');
-   });
-  }
-});
+      chrome.notifications.create(notifId, {
+        title : "New error",
+        message : errors.pop(),
+        type : "basic",
+        iconUrl : "icon.png"
+      }, function() {
+        console.log('Creation callback');
+      });
+    }
+  });
 
-  //chrome.alarms.create("alarm_id", {
-  //  delayInMinutes : 1,
-  //  periodInMinutes : 1
- // });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+
+ do_notifications();
+
 });

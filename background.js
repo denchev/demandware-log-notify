b2742d24c1bca2823a9d3751c56e1c11b2fedb2f
Marush Denchev <avreon@gmail.com>
2014-04-29 21:00:26 +0300

    Look for the selected types of log errors. Stable at this point.

--- background.js
+++ background.js
@@ -6,13 +6,12 @@ function make_base_auth(user, password) {
 
 var settings = {};
 
-function do_notifications() {
-
+function start_watch(log_type) {
   var d = new Date(), year = d.getFullYear(), month = ('0' + (d.getMonth()+1)).slice(-2),date = d.getDate();
 
   $.ajax({
     type: "GET",
-    url: "https://" + settings.server + "/on/demandware.servlet/webdav/Sites/Logs/error-blade" + settings['blade-1'] + "-" + settings['blade-2'] + "-appserver-" + year + month + date + ".log",
+    url: "https://" + settings.server + "/on/demandware.servlet/webdav/Sites/Logs/" + log_type + "-blade" + settings['blade-1'] + "-" + settings['blade-2'] + "-appserver-" + year + month + date + ".log",
     beforeSend: function (xhr){ 
         xhr.setRequestHeader('Authorization', make_base_auth(settings.username, settings.password)); 
     },
@@ -25,18 +24,26 @@ function do_notifications() {
 
       var errors = response.split(regex);
       var date = new Date();
-      var notifId = "" + date.getFullYear() + '_' + date.getMonth() + '_' + date.getDate() + '_' + errors.length + Math.random();
+      var notifId = "" + date.getFullYear() + '_' + date.getMonth() + '_' + date.getDate() + '_' + errors.length;
 
       chrome.notifications.create(notifId, {
-        title : "New error",
+        title : "New log change: " + log_type,
         message : errors.pop(),
         type : "basic",
         iconUrl : "icon.png"
       }, function() {
-        console.log('Creation callback');
       });
     }
   });
+}
+
+function do_notifications() {
+
+  for(var i = 0, n = logs_available.length; i < n; i+=1) {
+    if(settings['log-' + logs_available[i]]) {
+      start_watch(logs_available[i]);
+    }
+  }
 
 }
 
--- settings_list.js
+++ settings_list.js
@@ -1,1 +1,2 @@
 var settings_list = ['server', 'username', 'password', 'blade-1', 'blade-2', 'log-error', 'log-debug', 'log-customerror', 'log-customfatal', 'log-customwarn', 'log-custominfo'];
+var logs_available = ['error', 'debug', 'customerror', 'customfatal', 'customwarn', 'custominfo'];

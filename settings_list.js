02b61a588e13897692bf9266d78e0780bb4e0652
Marush Denchev <avreon@gmail.com>
2014-04-30 09:28:51 +0300

    More logs to watch. UI improvements.

--- settings_list.js
+++ settings_list.js
@@ -1,2 +1,3 @@
-var settings_list = ['server', 'username', 'password', 'blade-1', 'blade-2', 'log-error', 'log-debug', 'log-customerror', 'log-customfatal', 'log-customwarn', 'log-custominfo'];
-var logs_available = ['error', 'debug', 'customerror', 'customfatal', 'customwarn', 'custominfo'];
+var settings_list = ['server', 'username', 'password', 'blade-1', 'blade-2', 
+'log-error', 'log-debug', 'log-customerror', 'log-customfatal', 'log-customwarn', 'log-custominfo', 'log-jobs', 'log-quota', 'log-warn', 'api'];
+var logs_available = ['error', 'debug', 'customerror', 'customfatal', 'customwarn', 'custominfo', 'jobs', 'quota', 'warn', 'api'];
